8a826dac545b746558f7e80a165d32d28e615fb4
PodilapuPolinaidu <polinaidupodilapu2@gmail.com>
2026-02-22 20:29:20 +0530

    changed Fibonacci

--- FibnocciNumber.js
+++ FibnocciNumber.js
@@ -1,11 +1,23 @@
 let n = 8;
+// function fibnoccoNumber(n) {
+//   if (n == 0) {
+//     return 0;
+//   } else if (n == 1) {
+//     return 1;
+//   }
+//   return fibnoccoNumber(n - 1) + fibnoccoNumber(n - 2);
+// }
+
 function fibnoccoNumber(n) {
-  if (n == 0) {
-    return 0;
-  } else if (n == 1) {
-    return 1;
+  let n1 = 0;
+  let n2 = 1;
+  console.log(n1);
+  // console.log(n2);
+  let n3 = 0;
+  while (n) {
+    n3 = n1 + n2;
+
   }
-  return fibnoccoNumber(n - 1) + fibnoccoNumber(n - 2);
 }
 let res = fibnoccoNumber(n);
 console.log(res);
