8cc0c11d4f019b2dbb458ada89d08a001e3ef258
PodilapuPolinaidu <polinaidupodilapu2@gmail.com>
2026-02-25 21:07:26 +0530

    completed selection sort

---
+++ SelectionSort.js
@@ -0,0 +1,16 @@
+let arr = [7, 1, 5, 4, 3, 2];
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    let tem = arr[i];
+    arr[i] = arr[min];
+    arr[min] = tem;
+  }
+  console.log(arr);
+}
+selectionSort(arr);
