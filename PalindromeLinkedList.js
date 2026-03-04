ec774c2587bf9b427330dd5c0270c10fcdc9f1ca
PodilapuPolinaidu <polinaidupodilapu2@gmail.com>
2026-03-04 08:01:55 +0530

    Palindrome Linked list

---
+++ PalindromeLinkedList.js
@@ -0,0 +1,31 @@
+function palindrome(head) {
+  let slow = head;
+  let fast = head;
+  while (fast || fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+
+  let pre = null;
+  let cur = slow;
+  while (cur) {
+    let temp = cur.next;
+    cur.next = pre;
+    pre = cur;
+    cur = temp;
+  }
+
+  let firstlist = head;
+  let secondlist = pre;
+
+  while (secondlist) {
+    if (firstlist.value != secondlist.value) {
+      return false;
+    }
+
+    secondlist = secondlist.next;
+    firstlist = firstlist.next;
+  }
+
+  return true;
+}
